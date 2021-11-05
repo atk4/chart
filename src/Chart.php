0e40061e65f63d52dcd5d3db128134b79328c7ea
mkrecek234 <michael@krecek.net>
2021-11-05 16:41:17 +0100

    Add further colour for longer datasets

--- src/Chart.php
+++ src/Chart.php
@@ -31,6 +31,7 @@ class Chart extends View
         ['rgba(75, 192, 192, 0.2)', 'rgba(75, 192, 192, 1)'],
         ['rgba(153, 102, 255, 0.2)', 'rgba(153, 102, 255, 1)'],
         ['rgba(255, 159, 64, 0.2)', 'rgba(255, 159, 64, 1)'],
+        ['rgba(20, 20, 20, 0.2)', 'rgba(20, 20, 20, 1)']
     ];
 
     /** @var array Options for chart.js widget */
